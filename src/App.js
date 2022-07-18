28d27034deb0259e73742f5f796cbe25bae98366
brandib222 <brandi.danielle.ball@gmail.com>
2022-07-18 14:05:22 -0400

    built dashboard skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <p> Mom's Salon </p>
+      <Dashboard />
     </div>
   );
 }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ClientCard from './ClientCard';
+
+const initialClients = [];
+
+export default function Dashboard (props) {
+
+    const [clients, setClients] = useState(initialClients);
+
+    const getClients = () => {
+        axios.get('http://localhost:3000/')
+            .then(res => {
+                //setClients(res.data)
+                console.log(res);
+            }).catch(err => console.error(err));
+
+    }
+
+    useEffect(() => {
+        getClients();
+    }, [clients])
+
+    return (
+        <div className='clients-list-wrapper'> 
+            <div> Is it working </div>
+             {/* <h2 className='dashboard-h2'>Dashboard</h2>
+                <div className='client-cards'>
+                    {clients.map(client => (
+                        <ClientCard key={client.email} details={client} />
+                    ))}
+                </div> */}
+        </div>
+    )
+}
