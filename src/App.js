0702dd9d170a897bec1c5ea19b18a6a275a31cab
Brandi Ball <brandiball@Brandis-Mini.attlocal.net>
2022-11-10 11:11:35 -0500

    added css styling and fonts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/Dashboard';
 function App() {
   return (
     <div className="App">
+      <link href="https://fonts.googleapis.com/css2?family=Xanh+Mono&display=swap" rel="stylesheet"></link>
       <Dashboard />
     </div>
   );
